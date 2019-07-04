ad811caae66d1da6000472f321676242587475be
Leonardo Mosciatti <leonardo.mosciatti@gmail.com>
2019-07-04 17:02:10 +0200

    Added shipping page.

---
+++ public/assets/js/page_shipping.js
@@ -0,0 +1,24 @@
+
+var scriptsToLoad = [
+  'utils.js',
+  'Event.js',
+  'UserModel.js',
+  'UserNavbarView.js',
+  'UserNavbarController.js',
+  'BreadcrumbsModel.js',
+  'BreadcrumbsView.js',
+];
+
+$(function () {
+  $.when.apply($, scriptsToLoad.map(function(scriptToLoad){return $.loadScript('/assets/js/'+scriptToLoad)})).done(function () {
+    var userModel = new UserModel();
+    var userNavbarView = new UserNavbarView(userModel);
+    var userNavbarController = new UserNavbarController(userModel, userNavbarView);
+    var breadcrumbsModel = new BreadcrumbsModel();
+    var breadcrumbsView = new BreadcrumbsView(breadcrumbsModel);
+
+    userModel.getDetails();
+    breadcrumbsModel.addBreadcrumb('Home', '/index.html');
+    breadcrumbsModel.addBreadcrumb('Orders & Shipping', '/pages/shipping.html', true);
+  });
+});
